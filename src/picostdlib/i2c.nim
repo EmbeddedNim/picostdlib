a42b3bc943223c42c1e575145537973e0dfaa701
Jason <beefers331@gmail.com>
2021-09-14 02:50:55 -0600

    Added a 'setupGpio' template and fixed wrapped i2c stuff

--- src/picostdlib/i2c.nim
+++ src/picostdlib/i2c.nim
@@ -23,8 +23,8 @@ type
     hw*: ptr I2Hw
     restartOnNext*: bool
 
-var i2c0*: I2cInst {.importC: "i2c0_inst".}
-var i2c1*: I2cInst {.importC: "i2c1_inst".}
+var i2c0* {.importC: "i2c0_inst".}: I2cInst
+var i2c1* {.importC: "i2c1_inst".}: I2cInst
 
 proc init*(i2c: var I2cInst, baudrate: cuint) {.importC: "i2c_init".}
 proc deinit*(i2c: var I2cInst) {.importc: "i2c_deinit".}
