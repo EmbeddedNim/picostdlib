f1f17cca8bd486c85174e0a4d27e357946375bbb
Jason <beefers331@gmail.com>
2021-09-14 18:33:29 -0600

    added read/write blocking

--- src/picostdlib/i2c.nim
+++ src/picostdlib/i2c.nim
@@ -31,4 +31,10 @@ proc deinit*(i2c: var I2cInst) {.importc: "i2c_deinit".}
 proc setBaudrate*(i2c: var I2cInst, baudRate: cuint): cuint {.importc: "i2c_set_baudrate".}
 proc setSlaveMode*(i2c: var I2cInst, slave: bool, address: uint8) {.importc: "i2c_set_slave_mode".}
 
+proc writeBlocking*(i2c: var I2cInst, address: uint8, data: pointer, len: csize_t,
+    noStop: bool){.importc: "i2c_write_blockin".}
+
+proc readBlocking*(i2c: var I2cInst, address: uint8, dest: pointer, size: csize_t,
+    noStop: bool): cint {.importC: "i2c_read_blocking".}
+
 {.pop.}
