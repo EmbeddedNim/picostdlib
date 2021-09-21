85c2521df316b77d3da6b14dbb39dea55a325c71
Martinix75 <fintobersaglio@yahoo.it>
2021-09-21 10:50:15 +0200

    modify the spaces try2

--- examples/hello_i2c.nim
+++ examples/hello_i2c.nim
@@ -19,17 +19,17 @@ type
     blockk: I2cInst
     buffer: uint8
 
-proc writeBytex(self: Pcf8574, dato:uint8 ) = #proc to write the byte 
+proc writeBytex(self: Pcf8574, dato: uint8 ) = #proc to write the byte 
   let dato = dato.unsafeAddr #get the address of the data
-  writeBlocking(self.blockk, self.addressDevice, dato,1, true) #write the data on the i2c bus 
+  writeBlocking(self.blockk, self.addressDevice, dato, 1, true) #write the data on the i2c bus 
 
-proc digitaWrite(self:Pcf8574,pin:uint8, value:bool) =
+proc digitaWrite(self:Pcf8574, pin:uint8, value: bool) =
   if value == on:
     self.buffer = (self.buffer or pin) #go to act (turn on) the selected bit 
-    writeBytex(self,self.buffer)
+    writeBytex(self, self.buffer)
   elif value == off:
     self.buffer = (self.buffer and pin) #go to act (turn off) the selected bit 
-    writeBytex(self,self.buffer)
+    writeBytex(self, self.buffer)
 
 
 when isMainModule:
@@ -39,22 +39,22 @@ when isMainModule:
   const sda = 0.Gpio 
   const scl = 1.Gpio 
   const address = 0x20
-  init(i2c0,10000)
+  init(i2c0, 10000)
   sda.setFunction(I2C); sda.pullUp()
   scl.setFunction(I2C); scl.pullUp()
 
-  var buffer:uint8 = 0b00000000
+  var buffer: uint8 = 0b00000000
   writeBytex(expander, buffer)
   sleep(1000)
 
   while true:
-    digitaWrite(expander,p1,on) #turn on the bit "p1" 
+    digitaWrite(expander, p1, on) #turn on the bit "p1" 
     sleep(1500)
-    digitaWrite(expander,p4,on) #turn on the bit "p4" 
+    digitaWrite(expander, p4, on) #turn on the bit "p4" 
     sleep(1500)
-    digitaWrite(expander,p1,off) #turn off the bit "p1" 
+    digitaWrite(expander, p1, off) #turn off the bit "p1" 
     sleep(1500)
-    digitaWrite(expander,p4,off) #turn off the bit "p4" 
+    digitaWrite(expander, p4, off) #turn off the bit "p4" 
     sleep(1500)
     
 #[ in ...csource/CMakeLists.txt add target_link_libraries(tests pico_stdlib hardware_adc) 
