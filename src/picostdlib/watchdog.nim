efe6bd9cef46f34e749afdfe40b29d3181258124
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2021-10-27 17:18:49 +0200

    add watchdogReboot

--- src/picostdlib/watchdog.nim
+++ src/picostdlib/watchdog.nim
@@ -1,9 +1,11 @@
 {.push header: "hardware/watchdog.h".}
 
-proc watchdogEnable*(delayMs: uint32, pauseOnDebug: bool) {.importC: "watchdog_enable".}
+proc watchdogEnable*(delayms: uint32, pauseOnDebug: bool) {.importC: "watchdog_enable".}
 
 proc watchdogUpade*() {.importC: "watchdog_update".}
 
-proc watchdogCauseReboot*(): bool {.importC: "watchdog_caused_reboot".}
+proc watchdogCausedReboot*(): bool {.importC: "watchdog_caused_reboot".}
+
+proc watchdogReboot*(pc: uint32, sp: uint32, delayms: uint32) {.importC: "watchdog_reboot".}
 
 {.pop.}
