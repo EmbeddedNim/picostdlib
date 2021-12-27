2098ff379b1dfbadb4841c6d347846e4b17b7df0
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2021-12-27 13:12:19 +0100

    bug1

--- examples/hellotimestart.nim
+++ examples/hellotimestart.nim
@@ -5,10 +5,10 @@ stdioInitAll()
 var oldTime: uint32 = 0
 while true:
   var timeStart = timeUs32()  #it gets from how long the microcontroller is turned on (32bit, 4294 s)
-  oldTime = timeStart #store the current time in the variable 
   var seconds = float(float(timeStart) / 1000000.0) #convert from microseconnds to seconds
   print("Time Now : " & $seconds & '\n')
   print("Wait 2 seconds.." & '\n')
-  if timeStart < timeStart:
+  if timeStart <= oldTime:
     print("The time is restart from 0" & '\n')
+  oldTime = timeStart #store the current time in the variable 
   sleep(2000) #wait 2 seconds
