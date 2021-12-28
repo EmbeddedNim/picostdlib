37d3616a0e4deb2588cd089699dc32bd31b46de2
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2021-12-28 12:05:57 +0100

    add
    Added a simple example on the use of "timeus32 ()"

--- examples/hellotimestart.nim
+++ examples/hellotimestart.nim
@@ -2,13 +2,10 @@ import picostdlib/[gpio, time]
 import picostdlib
 
 stdioInitAll()
-var oldTime: uint32 = 0
+
 while true:
   var timeStart = timeUs32()  #it gets from how long the microcontroller is turned on (32bit, 4294 s)
-  var seconds = float(float(timeStart) / 1000000.0) #convert from microseconnds to seconds
+  var seconds = (float(timeStart) / 1000000.0) #convert from microseconnds to seconds
   print("Time Now : " & $seconds & '\n')
   print("Wait 2 seconds.." & '\n')
-  if timeStart <= oldTime:
-    print("The time is restart from 0" & '\n')
-  oldTime = timeStart #store the current time in the variable 
   sleep(2000) #wait 2 seconds
