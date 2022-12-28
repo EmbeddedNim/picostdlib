cc7cb6d7919099105eb3f0fbf367f5757ab150ac
daniel-j <daniel.j.hede@gmail.com>
2022-12-28 23:08:28 +0100

    add mbedtls_hardware_poll function

--- src/picostdlib/lib/mbedtls/ssl.nim
+++ src/picostdlib/lib/mbedtls/ssl.nim
@@ -1,3 +1,28 @@
+
+import ../../hardware/structs/rosc
+import ../../pico/platform
+
+# converted to Nim from https://github.com/raspberrypi/pico-sdk/pull/1151
+
+var pollByte: uint8
+
+# Function to feed mbedtls entropy.
+proc mbedtlsHardwarePoll*(data: pointer; output: ptr UncheckedArray[uint8]; len: csize_t; olen: ptr csize_t): cint {.exportc: "mbedtls_hardware_poll", noconv.} =
+  ## Code borrowed from pico_lwip_random_byte(), which is static, so we cannot call it directly
+
+  for p in 0..<len:
+    for i in 0..<32:
+      ## picked a fairly arbitrary polynomial of 0x35u - this doesn't have to be crazily uniform.
+      pollByte = (((pollByte shl 1) or roscHw.randombit) xor (if (pollByte and 0x80'u).bool: 0x35'u else: 0'u)).uint8
+      ## delay a little because the random bit is a little slow
+      busyWaitAtLeastCycles(30)
+
+    output[p] = pollByte;
+
+  olen[] = len
+  return 0
+
+
 {.push header: "mbedtls/ssl.h".}
 
 type
