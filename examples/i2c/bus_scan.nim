186f700b7f6f70de710a31cffe6b772377d8ada9
daniel-j <daniel.j.hede@gmail.com>
2023-03-05 15:57:44 +0100

    binary_info fix

--- examples/i2c/bus_scan.nim
+++ examples/i2c/bus_scan.nim
@@ -16,10 +16,9 @@ gpioSetFunction(DefaultI2cSclPin, I2c)
 gpioPullUp(DefaultI2cSdaPin)
 gpioPullUp(DefaultI2cSclPin)
 
-
 # Make the I2C pins available to picotool
-{.emit: "#include \"pico/binary_info.h\"".}
-{.emit: ["bi_decl(bi_2pins_with_func(", DefaultI2cSdaPin, ", ", DefaultI2cSclPin, ", ", I2c, "));"].}
+bi_decl_include()
+bi_decl(bi_2pins_with_func(DefaultI2cSdaPin, DefaultI2cSclPin, I2c))
 
 echo "I2C Bus Scan"
 echo "   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F"
