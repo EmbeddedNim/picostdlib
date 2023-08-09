4003f8de55ca0a2c546d49d3ba0720b68795db30
daniel-j <daniel.j.hede@gmail.com>
2023-08-09 16:16:16 +0200

    fix ssid length

--- examples/pico_w/picow_wifi_scan.nim
+++ examples/pico_w/picow_wifi_scan.nim
@@ -5,12 +5,13 @@ import picostdlib/pico/cyw43_arch
 
 proc scanResult(env: pointer; res: ptr Cyw43EvScanResultT): cint {.cdecl.} =
   if not res.isNil:
-    let ssid = cast[cstring](res.ssid[0].addr)
+    var ssid = newString(res.ssidLen)
+    copyMem(ssid[0].addr, res.ssid[0].addr, res.ssidLen)
     echo(
       "ssid: ", ssid,
       " rssi: ", res.rssi,
       " chan: ", res.channel,
-      " mac: ", res.bssid[0].toHex, ":", res.bssid[1].toHex, ":", res.bssid[2].toHex, ":", res.bssid[3].toHex, ":", res.bssid[4].toHex, ":", res.bssid[5].toHex,
+      " bssid: ", res.bssid[0].toHex, ":", res.bssid[1].toHex, ":", res.bssid[2].toHex, ":", res.bssid[3].toHex, ":", res.bssid[4].toHex, ":", res.bssid[5].toHex,
       " sec: ", res.authMode
     )
   return 0
