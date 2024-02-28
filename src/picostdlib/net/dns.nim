a33e891d444159452080fbf418d7a330decb41db
daniel-j <daniel.j.hede@gmail.com>
2024-02-28 17:24:27 +0100

    dns callbacks is of limited size

--- src/picostdlib/net/dns.nim
+++ src/picostdlib/net/dns.nim
@@ -17,24 +17,40 @@ type
 
   DnsCallback* = proc(hostname: string; ipaddr: ptr IpAddrT)
 
+var dnsCbSlots: array[DNS_MAX_REQUESTS, DnsCallback]
+
+proc getSlot(): int =
+  for i, slot in dnsCbSlots:
+    if slot == nil:
+      return i
+  return -1
+
 proc dnsGethostbynameCb(hostname: cstring; ipaddr: ptr IpAddrT; arg: pointer) {.cdecl.} =
-  let state = cast[ref DnsCbState](arg)
-  state.callback($hostname, ipaddr)
-  reset(state.callback)
-  GC_unref(state)
+  let slotIndex = cast[int](arg)
+  var callback = dnsCbSlots[slotIndex] #cast[ref DnsCbState](arg)
+  if callback == nil: return
+  callback($hostname, ipaddr)
+  reset(callback)
+  callback = nil
+  #GC_unref(state)
 
 proc getHostByName*(hostname: string; callback: DnsCallback; timeoutMs: Natural = 5000): bool =
   var err: ErrEnumT
   var ipaddrIn: IpAddrT
 
   withLwipLock:
-    var state = new(DnsCbState)
-    state.callback = callback
+    var slotIndex = getSlot()
+    if slotIndex < 0:
+      debugv(":dns callback count limit reached")
+      return false
+    #var state = new(DnsCbState)
+    #state.callback = callback
 
-    err = dnsGethostbyname(hostname.cstring, ipaddrIn.addr, dnsGethostbynameCb, cast[pointer](state)).ErrEnumT
+    err = dnsGethostbyname(hostname.cstring, ipaddrIn.addr, dnsGethostbynameCb, cast[pointer](slotIndex)).ErrEnumT
 
     if err == ErrInprogress:
-      GC_ref(state)
+      dnsCbSlots[slotIndex] = callback
+      #GC_ref(state)
       return true
     elif err != ErrOk:
       debugv(":dns err=" & $err & " " & hostname)
